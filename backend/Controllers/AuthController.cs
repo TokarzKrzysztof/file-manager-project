8070cccc0f0e8878a45ffa35c0e2d2d29f134d37
Krzysiek1472 <krzysiek1472@gmail.com>
2020-07-14 20:33:19 +0200

    file manager first commit

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+
+    }
+}
