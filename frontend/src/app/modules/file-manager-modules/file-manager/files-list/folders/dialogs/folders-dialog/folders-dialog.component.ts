5b013af574262d80bd587376a3114846a6b0e3aa
Krzysiek1472 <krzysiek1472@gmail.com>
2020-07-22 23:22:36 +0200

    nested tree new progress

--- frontend/src/app/modules/file-manager-modules/file-manager/files-list/folders/dialogs/folders-dialog/folders-dialog.component.ts
+++ frontend/src/app/modules/file-manager-modules/file-manager/files-list/folders/dialogs/folders-dialog/folders-dialog.component.ts
@@ -3,12 +3,14 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FolderModel } from 'src/app/modules/file-manager-modules/model-FolderModel';
+import { Observable } from 'rxjs';
+import { startWith, filter, map } from 'rxjs/operators';
 
 export interface AddFolderData {
   title: string;
-  withParent?: boolean;
   editedFolderId?: number;
   parentId?: number;
+  flatenedFolders?: { id: number, name: string }[];
 }
 
 @Component({
@@ -17,6 +19,8 @@ export interface AddFolderData {
   styleUrls: ['./folders-dialog.component.scss']
 })
 export class FoldersDialogComponent implements OnInit {
+  filteredFolders: Observable<{ id: number, name: string }[]>;
+
   formGroup = new FormGroup({
     parentId: new FormControl(null),
     name: new FormControl('', [Validators.required])
@@ -29,14 +33,35 @@ export class FoldersDialogComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    if (this.data.withParent) {
-      this.formGroup.get('parentId').setValidators(Validators.required);
+    if (this.data.parentId) {
+      const parentIdForm: FormControl = this.formGroup.get('parentId') as FormControl;
+      parentIdForm.setValidators(Validators.required);
+      parentIdForm.setValue(this.data.parentId);
+
+      this.filteredFolders = parentIdForm.valueChanges.pipe(
+        startWith(''),
+        filter(value => typeof value === 'string'),
+        map((value: string) => this.data.flatenedFolders.filter(x => x.name.toLowerCase().includes(value.toLowerCase())))
+      );
+    }
+
+  }
+
+  displayFn(folderId: number): string {
+    const folder: FolderModel = this.data.flatenedFolders.find(x => x.id === folderId);
+    if (folder) {
+      return folder.name;
     }
   }
 
   onAccept() {
     if (this.formGroup.invalid) {
       this.toast.error('Wypełnij poprawnie wszystkie pola!');
+      return;
+    }
+
+    if (typeof this.formGroup.get('parentId').value === 'string') {
+      this.toast.error('Wybierz poprawny folder nadrzędny!');
       return;
     }
 
--- frontend/src/app/modules/file-manager-modules/file-manager/files-list/folders/folders.component.ts
+++ frontend/src/app/modules/file-manager-modules/file-manager/files-list/folders/folders.component.ts
@@ -6,60 +6,12 @@ import { MatDialog } from '@angular/material/dialog';
 import { FolderModel } from '../../../model-FolderModel';
 import { FoldersService } from '../../../folders.service';
 import { FoldersDialogComponent, AddFolderData } from './dialogs/folders-dialog/folders-dialog.component';
-
-// interface FoldersNode {
-//   name: string;
-//   children?: FoldersNode[];
-// }
-
-// const TREE_DATA: FoldersNode[] = [
-//   {
-//     name: 'Fruit',
-//     children: [
-//       { name: 'Apple' },
-//       { name: 'Banana' },
-//       { name: 'Fruit loops' },
-//     ]
-//   }, {
-//     name: 'Vegetables',
-//     children: [
-//       {
-//         name: 'Green',
-//         children: [
-//           { name: 'Broccoli' },
-//           { name: 'Brussels sprouts' },
-//         ]
-//       }, {
-//         name: 'Orange',
-//         children: [
-//           { name: 'Pumpkins' },
-//           {
-//             name: 'Carrots', children: [
-//               { name: 'asdas' },
-//               {
-//                 name: 'asas', children: [
-//                   {
-//                     name: 'aaaa', children: [
-//                       {
-//                         name: 'asdasdasdsasad', children: [
-//                           { name: 'asdassdasdasdsadasd' }
-//                         ]
-//                       }
-//                     ]
-//                   }
-//                 ]
-//               }
-//             ]
-//           },
-//         ]
-//       },
-//     ]
-//   },
-// ];
+import { ConfirmationDialogData, ConfirmationDialogComponent } from 'src/app/shared/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 interface FlatNode {
-  expandable: boolean;
+  id: number;
   name: string;
+  expandable: boolean;
   level: number;
 }
 
@@ -69,6 +21,7 @@ interface FlatNode {
   styleUrls: ['./folders.component.scss']
 })
 export class FoldersComponent implements OnInit {
+  expandedNodes: FlatNode[] = [];
   treeControl = new FlatTreeControl<FlatNode>(node => node.level, node => node.expandable);
   treeFlattener = new MatTreeFlattener(this.transformer, node => node.level, node => node.expandable, node => node.children);
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
@@ -78,17 +31,33 @@ export class FoldersComponent implements OnInit {
     private foldersService: FoldersService
   ) {
   }
-  
+
   async ngOnInit() {
+    await this.loadFolders();
+  }
+
+  async loadFolders() {
+    this.expandedNodes = [];
+    this.treeControl.dataNodes?.forEach(node => {
+        if (this.treeControl.isExpanded(node)) {
+            this.expandedNodes.push(node);
+        }
+    });
+
     this.dataSource.data = await this.foldersService.getFoldersTree();
+
+    this.expandedNodes.forEach((node: FlatNode) => {
+      this.treeControl.expand(this.treeControl.dataNodes.find(x => x.id === node.id));
+    });
   }
 
   hasChild = (_: number, node: FlatNode) => node.expandable;
 
   private transformer(node: FolderModel, level: number) {
     const flatNode: FlatNode = {
-      expandable: !!node.children && node.children.length > 0,
+      id: node.id,
       name: node.name,
+      expandable: node.children?.length > 0,
       level,
     }
 
@@ -98,8 +67,6 @@ export class FoldersComponent implements OnInit {
   addRootFolder() {
     const dialogData: AddFolderData = {
       title: 'Dodaj folder nadrzędny',
-      withParent: false,
-      editingFolder: null
     };
 
     this.dialog.open(FoldersDialogComponent, {
@@ -107,6 +74,57 @@ export class FoldersComponent implements OnInit {
     }).afterClosed().subscribe(async (folderData: FolderModel) => {
       if (folderData !== null) {
         await this.foldersService.createFolder(folderData);
+        await this.loadFolders();
+      }
+    });
+  }
+
+  addSubfolder(folderId: number) {
+    const flatenedFolders: FlatNode[] = this.treeFlattener.flattenNodes(this.dataSource.data);
+
+    const dialogData: AddFolderData = {
+      title: 'Dodaj podfolder',
+      parentId: folderId,
+      flatenedFolders: flatenedFolders.map(x => ({ id: x.id, name: x.name }))
+    };
+
+    this.dialog.open(FoldersDialogComponent, {
+      data: dialogData
+    }).afterClosed().subscribe(async (folderData: FolderModel) => {
+      if (folderData !== null) {
+        await this.foldersService.createFolder(folderData);
+        await this.loadFolders();
+      }
+    });
+  }
+
+  editName(folderId: number) {
+    const dialogData: AddFolderData = {
+      title: 'Edytuj nazwę folderu',
+      editedFolderId: folderId
+    };
+
+    this.dialog.open(FoldersDialogComponent, {
+      data: dialogData
+    }).afterClosed().subscribe(async (folderData: FolderModel) => {
+      if (folderData !== null) {
+        await this.foldersService.updateFolder(folderData);
+        await this.loadFolders();
+      }
+    });
+  }
+
+  deleteFolder(folderId: number) {
+    const confirmationDialogData: ConfirmationDialogData = {
+      title: 'Czy na pewno chcesz usunąć ten folder, wszystkie podfoldery i pliki w nich zawarte?',
+    };
+
+    this.dialog.open(ConfirmationDialogComponent, {
+      data: confirmationDialogData
+    }).afterClosed().subscribe(async (result: boolean) => {
+      if (result) {
+        await this.foldersService.deleteFolder(folderId);
+        await this.loadFolders();
       }
     });
   }
