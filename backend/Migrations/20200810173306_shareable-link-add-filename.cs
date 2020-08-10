36485cd4148f9df576ed3e0bc557d3fdf308806c
Krzysiek1472 <krzysiek1472@gmail.com>
2020-08-10 19:40:59 +0200

    add migration shareable link

---
+++ backend/Migrations/20200810173306_shareable-link-add-filename.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class shareablelinkaddfilename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "ShareableLinks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileName",
+                table: "ShareableLinks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileName",
+                table: "ShareableLinks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "ShareableLinks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
